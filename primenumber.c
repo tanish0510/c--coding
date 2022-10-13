c9258df3559998923f798e6175c838589f684323
tanish0510 <tanishseth0510@gmail.com>
2022-10-13 11:09:00 +0530

    prime number

---
+++ primenumber.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int i,n=1;
+    printf("prime nubers between 1-300 are");
+    for(n=1;n<=300;n++)
+    {
+        i=2;
+    for(i=2;i<n;i++)
+        {
+            if(n%i==0)
+            break;
+        }
+    if(i==n)
+    printf("%d\t",n);
+    }
+    return 0;
+
+
+}
