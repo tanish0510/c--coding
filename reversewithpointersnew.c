5988a4e2c8ec4475c5ab2cd50a600151bbf3aac0
tanish0510 <tanishseth0510@gmail.com>
2023-02-07 22:15:31 +0530

    ft

--- reversewithpointersnew.c
+++ reversewithpointersnew.c
@@ -1,17 +1,23 @@
 #include<stdio.h>
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 #define N 5
 int main()
 {
     int a[5], *p;
     printf("enter  elememts in the array:", N);
 =======
+=======
+>>>>>>> Stashed changes
 
 int main()
 {
     int a[5], *p,N[6];
     printf("enter  elememts in the array:");
     scanf("%d", &N[6]);
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
     for(p=a; p<=a+N-1; p++)
     {
